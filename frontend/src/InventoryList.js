9fb4b4d0e5c5e45b8862dbacaa40141ccfe1537c
Cassia Silva <cassia.csilva12@gmail.com>
2024-09-23 23:57:58 -0300

    inclusão de layout e formatações

--- frontend/src/InventoryList.js
+++ frontend/src/InventoryList.js
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './InventoryList.css';
 
 const InventoryList = () => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState('');
   const [newItem, setNewItem] = useState({ name: '', quantity: '', price: '' });
   const [editItem, setEditItem] = useState(null);
+
+  const handleEditClick = (item) => {
+    setEditItem(item); // Preenche o formulário de edição com o item selecionado
+  };
+
+  const formatCurrency = (value) => {
+    const cleanValue = value.replace(/\D/g, '');
+    
+    const numberValue = (parseInt(cleanValue, 10) / 100).toFixed(2);
+    
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(numberValue);
+  };
+  
+  const cleanCurrencyValue = (value) => {
+    // Remove o "R$" e os separadores de milhares (pontos e vírgulas)
+    return parseFloat(value.replace(/\D/g, '')) / 100;
+  };
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -25,7 +46,6 @@ const InventoryList = () => {
       await axios.delete(`http://localhost:5000/api/inventory/items/${id}`);
       setItems(items.filter(item => item._id !== id));
     } catch (err) {
-      console.error(err);
       setError('Erro ao deletar item');
     }
   };
@@ -33,7 +53,8 @@ const InventoryList = () => {
   const handleAddItem = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/inventory/items', newItem);
+      const cleanedPrice = cleanCurrencyValue(newItem.price);
+      const response = await axios.post('http://localhost:5000/api/inventory/items', { ...newItem, price: cleanedPrice });
       setItems([...items, response.data]);
       setNewItem({ name: '', quantity: '', price: '' });
     } catch (err) {
@@ -44,7 +65,8 @@ const InventoryList = () => {
   const handleEditItem = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.put(`http://localhost:5000/api/inventory/items/${editItem._id}`, editItem);
+      const cleanedPrice = cleanCurrencyValue(editItem.price);
+      const response = await axios.put(`http://localhost:5000/api/inventory/items/${editItem._id}`, { ...editItem, price: cleanedPrice });
       setItems(items.map(item => (item._id === editItem._id ? response.data : item)));
       setEditItem(null);
     } catch (err) {
@@ -53,10 +75,11 @@ const InventoryList = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Controle e Gerenciamento de Estoque</h1>
-      {error && <p>{error}</p>}
-      
+      {error && <p className="error-message">{error}</p>}
+
+      {/* Formulário de Adicionar Produto */}
       <h2>Adicionar Produto</h2>
       <form onSubmit={handleAddItem}>
         <input
@@ -74,29 +97,35 @@ const InventoryList = () => {
           required
         />
         <input
-          type="number"
+          type="text"
           placeholder="Preço"
           value={newItem.price}
-          onChange={(e) => setNewItem({ ...newItem, price: e.target.value })}
+          onChange={(e) => setNewItem({ ...newItem, price: formatCurrency(e.target.value) })} 
           required
         />
         <button type="submit">Adicionar</button>
       </form>
 
+      {/* Lista de Produtos */}
       <h2>Lista de Produtos</h2>
       <ul>
         {items.map(item => (
-          <li key={item._id}>
-            {item.name} - {item.quantity} - {item.price}
-            <button onClick={() => { setEditItem(item); setNewItem({ name: item.name, quantity: item.quantity, price: item.price }); }}>Editar</button>
-            <button onClick={() => handleDelete(item._id)}>Excluir</button>
+          <li key={item._id} style={{ marginBottom: '20px' }}>
+            <p><strong>Nome do produto:</strong> {item.name}</p>
+            <p><strong>Quantidade:</strong> {item.quantity}</p>
+            <p><strong>Preço:</strong> {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.price)}</p>
+            <div className="button-group">
+              <button onClick={() => handleEditClick(item)}>Editar</button>
+              <button onClick={() => handleDelete(item._id)}>Excluir</button>
+            </div>
           </li>
         ))}
       </ul>
 
+      {/* Formulário de Editar Produto */}
       {editItem && (
         <div>
-          <h2>Editar Item</h2>
+          <h2>Editar Produto</h2>
           <form onSubmit={handleEditItem}>
             <input
               type="text"
@@ -113,13 +142,13 @@ const InventoryList = () => {
               required
             />
             <input
-              type="number"
+              type="text"
               placeholder="Preço"
               value={editItem.price}
-              onChange={(e) => setEditItem({ ...editItem, price: e.target.value })}
+              onChange={(e) => setEditItem({ ...editItem, price: formatCurrency(e.target.value) })}
               required
             />
-            <button type="submit">Salvar</button>
+            <button type="submit">Salvar Alterações</button>
           </form>
         </div>
       )}
